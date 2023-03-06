a9b24389d301a23e9a433a525539aab571abcb72
vithuyquynh <voho39850@gmail.com>
2023-03-07 01:45:43 +0700

    update

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,19 @@
+import classNames from "classnames/bind";
+import Header from "~/components/Layout/components/Header";
+import SideBar from "./SideBar";
+import style from './DefaultLayout.module.scss'
+const cx = classNames.bind(style)
+function DefaultLayout({ children }) {
+
+    return (
+        <div className={cx('wrapper')}>
+            <Header></Header>
+            <div className={cx('container')}>
+                <SideBar></SideBar>
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind';
+import style from './Header.module.scss'
+const cx = classNames.bind(style)
+function Header() {
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+
+        </div>
+    </header>
+}
+
+export default Header;
